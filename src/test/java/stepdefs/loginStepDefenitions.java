1763a13d66cdb77d1483d5ac0fe4a4874bb29baf
Natalia <natalia.lyaskina@opencastsoftware.com>
2019-02-25 11:53:07 +0000

    Added Safari WebDeriver

--- src/test/java/stepdefs/loginStepDefenitions.java
+++ src/test/java/stepdefs/loginStepDefenitions.java
@@ -8,9 +8,13 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
+import java.lang.Exception;
+
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,14 +23,23 @@ public class loginStepDefenitions {
 
     WebDriver driver; // Created a variable called driver
 
-    @Before
+    @Before ("@full_coverage")
 
-    public void setupChromeDriver() throws IOException {
+    public void setupChromeDriver() throws Exception {
         System.setProperty("webdriver.chrome.driver", "/Users/natalialyaskina/Downloads/chromedriver");
         this.driver = new ChromeDriver();
         this.driver.manage().window().maximize();
         this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
         System.out.println("Chrome browser was opened");
+    }
+
+    @Before ("@positive")
+
+    public void setupSafaryDriver() throws Exception {
+        this.driver = new SafariDriver();
+        this.driver.manage().window().maximize();
+        this.driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
+        System.out.println("Safari browser was opened");
     }
 
     @After
@@ -39,10 +52,6 @@ public class loginStepDefenitions {
 
     }
 
-
-    //public WebDriver driver;
-    //static String driverPath = "/Users/natalialyaskina/Downloads/chromedriver";
-    //private String baseURL;
 
     @Given("^User navigates to WebDriverUniversity website$")
     public void user_navigates_to_Udemy_website() throws Exception
@@ -79,30 +88,30 @@ public class loginStepDefenitions {
     }
 
     @Given("^User enters a valid password$")
-    public void user_enters_a_valid_password() throws Throwable {
+    public void user_enters_a_valid_password() throws Exception {
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys("webdriver123");
         System.out.println("User entered valid password");
 
     }
 
     @When("^User clicks on the Login button$")
-    public void user_clicks_on_the_Login_button() throws Throwable {
+    public void user_clicks_on_the_Login_button() throws Exception {
         driver.findElement(By.xpath("//button[@id='login-button']")).click();
         System.out.println("User clicked on the login button");
         Thread.sleep(1000);
     }
 
     @Then("^User should be shown login success message$")
-    public void user_should_be_taken_to_the_success_login_page() throws Throwable {
+    public void user_should_be_taken_to_the_success_login_page() throws Exception {
         Alert alert = driver.switchTo().alert();
-        System.out.println(((Alert) alert).getText());
+        System.out.println("Login success/failure message: " + (alert).getText());
         assertEquals("validationsucceeded", alert.getText().toString().toLowerCase().replaceAll("\\s",""));
         alert.accept();
     }
 
 
     @Given("^User enters \"([^\"]*)\" username$")
-    public void user_enters_username(String username) throws Throwable {
+    public void user_enters_username(String username) throws Exception {
         //store the current window handle
         @SuppressWarnings("unused")
         String winHandleBefore = driver.getWindowHandle();
@@ -117,16 +126,16 @@ public class loginStepDefenitions {
     }
 
     @Given("^User enters \"([^\"]*)\" password$")
-    public void user_enters_password(String password) throws Throwable {
+    public void user_enters_password(String password) throws Exception {
         driver.findElement(By.xpath("//input[@id='password']")).sendKeys(password);
         System.out.println("User entered their password");
 
     }
 
     @Then("^User should be shown login \"([^\"]*)\" message$")
-    public void user_should_be_shown_login_message(String message) throws Throwable {
+    public void user_should_be_shown_login_message(String message) throws WebDriverException {
         Alert alert = driver.switchTo().alert();
-        System.out.println(((Alert) alert).getText());
+        System.out.println("Login success/failure message: " + (alert).getText());
         assertEquals(message.toLowerCase().replaceAll("\\s",""), alert.getText().toString().toLowerCase().replaceAll("\\s",""));
         alert.accept();
 
